46cba0c7ac1f08f1f714cbc134f5e2fd53526026
Gl4cier666 <damiensandoval15@gmail.com>
2017-09-20 18:48:31 -0700

    Flxed previous bug that crashed the app. Created activity for adding shows.

--- app/src/main/java/com/damienksandoval/www/showbookings/Shows.java
+++ app/src/main/java/com/damienksandoval/www/showbookings/Shows.java
@@ -1,5 +1,6 @@
 package com.damienksandoval.www.showbookings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,26 +10,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class Shows extends AppCompatActivity {
-
-    RecyclerView showView;
+    View showView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shows);
+        RecyclerAdapter rAdapter = new RecyclerAdapter();
+        showView = (RecyclerView)findViewById(R.id.show_list);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.activity_addshow);
+                startActivity(new Intent(Shows.this, AddShow.class));
             }
         });
-        RecyclerAdapter rAdapter = new RecyclerAdapter();
-        showView = (RecyclerView)findViewById(R.id.show_list);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
