c4c95bf5b81184c825f034808df097867a10b495
Gl4cier666 <damiensandoval15@gmail.com>
2017-09-27 14:37:14 -0700

    Rebuilt ShowItem class with new variables. Created method to retrieve data from date/time pickers.

--- app/src/main/java/com/damienksandoval/www/showbookings/AddShow.java
+++ app/src/main/java/com/damienksandoval/www/showbookings/AddShow.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class AddShow extends AppCompatActivity {
     public ShowItem newShow;
     private final Button saveShow = (Button) findViewById(R.id.saveShowButton);
     private DatePicker showStartDate = (DatePicker) findViewById(R.id.sDatePicker);
+    private DatePicker showEndDate = (DatePicker) findViewById(R.id.eDatePicker);
+    private TimePicker showStartTime = (TimePicker) findViewById(R.id.sTimePicker);
+    private TimePicker showEndTime = (TimePicker) findViewById(R.id.eTimePicker);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +27,18 @@ public class AddShow extends AppCompatActivity {
         });
     }
     //pull data from showStartDate
-    private void saveShowDate(int day, int month, int year){
-
+    private void saveShowDate(){
+        newShow.setSday(showStartDate.getDayOfMonth());
+        newShow.setSmonth(showStartDate.getMonth());
+        newShow.setSyear(showStartDate.getYear());
+        newShow.setEday(showEndDate.getDayOfMonth());
+        newShow.setEmonth(showEndDate.getMonth());
+        newShow.setEyear(showEndDate.getYear());
 
     }
+    private void saveShowTime(){
 
+    }
 
 
 
--- app/src/main/java/com/damienksandoval/www/showbookings/ShowItem.java
+++ app/src/main/java/com/damienksandoval/www/showbookings/ShowItem.java
@@ -1,6 +1,5 @@
 package com.damienksandoval.www.showbookings;
 
-import java.util.ArrayList;
 
 /**
  * ShowItem class is used for creating events. Events are currently stored in an ArrayList with
@@ -10,16 +9,35 @@ import java.util.ArrayList;
 public class ShowItem {
     private String showName;
     private int capacity;
-    private int date;//Need to change into 3 ints for spinner
-    private int time;
+    private int sday;
+    private int smonth;
+    private int syear;
+    private int eday;
+    private int emonth;
+    private int eyear;
+    private int shour;
+    private int sminute;
+    private int ehour;
+    private int eminute;
 
 
-    public ShowItem(String name, int venCapacity, int showDate, int showTime) {
+    public ShowItem(String name, int venCapacity, int sday, int smonth, int syear, int eday, int emonth, int eyear,
+                    int shour, int sminute, int ehour, int eminute) {
         this.showName = name;
         this.capacity = venCapacity;
-        this.date = showDate;
-        this.time = showTime;
+        this.sday = sday;
+        this.smonth = smonth;
+        this.syear = syear;
+        this.eday = eday;
+        this.emonth = emonth;
+        this.eyear = eyear;
+        this.shour = shour;
+        this.sminute = sminute;
+        this.ehour = ehour;
+        this.eminute = eminute;
     }
+
+    //REBUILD ALL SETTERS AND GETTERS
 
     public String getShowName() {
         return showName;
@@ -37,20 +55,67 @@ public class ShowItem {
         this.capacity = newCapacity;
     }
 
-    public int getDate() {
-        return date;
+    public int getHour() {
+        return hour;
     }
 
-    public void setDate(int newDate) {
-        this.date = newDate;
+    public void setHour(int hour) {
+        this.hour = hour;
     }
 
-    public int getTime(){
-        return time;
+    public int getMinute() {
+        return minute;
     }
 
-    public void setTime(int newTime){
-        this.time = newTime;
+    public void setMinute(int minute) {
+        this.minute = minute;
     }
 
+    public int getSday() {
+        return sday;
+    }
+
+    public void setSday(int sday) {
+        this.sday = sday;
+    }
+
+    public int getSmonth() {
+        return smonth;
+    }
+
+    public void setSmonth(int smonth) {
+        this.smonth = smonth;
+    }
+
+    public int getSyear() {
+        return syear;
+    }
+
+    public void setSyear(int syear) {
+        this.syear = syear;
+    }
+
+    public int getEday() {
+        return eday;
+    }
+
+    public void setEday(int eday) {
+        this.eday = eday;
+    }
+
+    public int getEmonth() {
+        return emonth;
+    }
+
+    public void setEmonth(int emonth) {
+        this.emonth = emonth;
+    }
+
+    public int getEyear() {
+        return eyear;
+    }
+
+    public void setEyear(int eyear) {
+        this.eyear = eyear;
+    }
 }
